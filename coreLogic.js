2ad4e1a00f765c35cdbcc4320efa926dfd90085d
alex <al@koii.network>
2023-02-26 01:44:27 -0400

    initial concept, working rest APIs

--- coreLogic.js
+++ coreLogic.js
@@ -1,24 +1,46 @@
 const { namespaceWrapper } = require("./namespaceWrapper");
 
-async function task() {
-  // Write the logic to do the work required for submitting the values and optionally store the result in levelDB
-  
-  // Below is just a sample of work that a task can do
-  
-  const x = Math.random().toString(); // generate random number and convert to string
-  const cid = crypto.createHash("sha1").update(x).digest("hex"); // convert to CID
-  console.log("HASH:", cid);
+let connection;
 
-  if (cid) {
-    await namespaceWrapper.storeSet("cid", cid); // store CID in levelDB
-  }
+async function task() {
+  // open RPC connection to get node list
+  if (!connection.getAccountInfo) connection = new web3.Connection(
+    web3.clusterApiUrl('devnet'),
+    'confirmed',
+  );
+
+  // fetch node list
+  // let nodeList = connection.; // TODO - add web3.js call to fetch validator list here
+  let nodeList = [{ id : "laksjdlsaksljdklaskj", address : "123.543.222.234" }]
+  namespaceWrapper.storeSet('nodes', nodeList)
+
+  nodeList.forEach( async (node) => {
+    /* ping each node and check 
+      balance of this task account
+      slot #
+      epoch #
+    */
+  
+    // get account info
+    let nodeData = {
+      account : await connection.getAccountInfo(namespaceWrapper.publicKey),
+      slot_id : await connection.getSlot(),
+      timestamp : await connection.getBlockTime(slot),
+      epoch : await connection.getEpochInfo()
+    }
+
+    // generate proof using local signature and add it to the node object
+    nodeData.signature = "signature"; // TODO - add signature + hash with slot # 
+
+    // update levelDb for the node
+    namespaceWrapper.storeSet(node.id, nodeData);
+  })
   
 }
 async function fetchSubmission(){
   // Write the logic to fetch the submission values here and return the cid string
 
   // The code below shows how you can fetch your stored value from level DB
-
   const cid = await namespaceWrapper.storeGet("cid"); // retrieves the cid
   return cid;
 }
@@ -88,17 +110,28 @@ async function submitDistributionList(round) {
 
 
 async function validateNode(submission_value) {
-  
-// Write your logic for the validation of submission value here and return a boolean value in response
 
-console.log("submission_value", submission_value);
-return true;
+  /* 
+    Rounds for this task are 1 minute, so we can establish consensus for very temporary data
+
+    TODO 
+    This function should verify another nodes work, there are several key checks
+    1. Wallet balance should match for all nodes during the round period (no transfers are allowed for staking wallets during function)
+    2. Health statuses should always match during rounds 
+    3. Overall node list should match
+    4. Overall epoch should match
+    5. Slot # must be between the slot the round started at and the slot the round ended at
+
+   */
+
+  console.log("submission_value", submission_value);
+  return true; // is true here "audit" or "pass"?
 }
 
 async function validateDistribution(distribution_list) {
 
-// Write your logic for the validation of submission value here and return a boolean value in response
-// this logic can be same as generation of distribution list function and based on the comparision will final object , decision can be made
+  // Write your logic for the validation of submission value here and return a boolean value in response
+  // this logic can be same as generation of distribution list function and based on the comparision will final object , decision can be made
   console.log("Validating Distribution Node", distribution_list);
   return true;
   
--- init.js
+++ init.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { namespaceWrapper } = require('./namespaceWrapper');
 const TASK_NAME = process.argv[2];
 const TASK_ID = process.argv[3];
 const EXPRESS_PORT = process.argv[4];
@@ -12,13 +13,36 @@ const STAKE = Number(process.argv[10]);
 const app = express();
 
 console.log('SETTING UP EXPRESS', NODE_MODE);
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', async (req, res) => {
+  let k2_diagnostics_report = await generateReport();
+  res.send(k2_diagnostics_report);
 });
 
 app.listen(EXPRESS_PORT, () => {
   console.log(`${TASK_NAME} listening on port ${EXPRESS_PORT}`);
 });
+
+const generateReport = async () => {
+  // fetch last N periods of historical data
+
+  let report = {
+    nodes : namespaceWrapper.storeGet('nodes'),
+    history : {},
+    healthChecks : []
+  }
+
+  report.nodes.forEach( async (node) => {
+    let nodeData = await namespaceWrapper.storeGet(node.id)
+    if (nodeData.is_leader) report.history = {
+      epoch : nodeData.epoch,
+      time : nodeData.timestamp,
+      last_slot : nodeData.slot_id
+    }
+    report.healthChecks.push(nodeData)
+  })
+
+  return report;
+}
 
 module.exports = {
   app,
